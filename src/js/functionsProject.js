749bd232d47ffcb32de0f3f5b2215e359cd58587
leopoldo araujo <lpaa83@gmail.com>
2020-03-07 16:51:24 -0300

    Landing page DSA full

---
+++ src/js/functionsProject.js
@@ -0,0 +1,43 @@
+//Fechar nav ao clicar
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+//Scroll Page
+$('a.scroll_nav').on('click',function (e) {
+    // e.preventDefault();
+    var target = this.hash,
+    $target = $(target);
+
+   $('html, body').stop().animate({
+     'scrollTop': $target.offset().top-50
+    }, 2500, 'swing', function () {
+     window.location.hash = target;
+    });
+});
+
+//slick menu
+$(window).on('scroll',function() {
+  var scrolltop = $(this).scrollTop();
+
+  if(scrolltop >= 40) {
+    $('.navClient').addClass('fixClient');
+  }
+
+  else if(scrolltop <= 40) {
+    $('.navClient').removeClass('fixClient');
+  }
+
+  if(scrolltop >= 1200) {
+      $(".animated").addClass("fadeInUp");
+  } 
+
+});
+
+//Galeria Tsunami
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox({
+        alwaysShowClose: true
+    });
+});
